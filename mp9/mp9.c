8310765c7129d88aa0503f4ba5f19243faa90404
Raventhatfly <feiyangw.21@intl.zju.edu.cn>
2022-11-29 15:35:21 +0800

    mp9 finished without comments

--- mp9/mp9.c
+++ mp9/mp9.c
@@ -21,10 +21,19 @@ find_nodes (locale_t* loc, vertex_set_t* vs, pyr_tree_t* p, int32_t nnum)
         }   
         return;
     }
-    if(/*loc->x <= p->node[nnum].x && loc->y <= p->node[nnum].y_left  &&*/ 4*nnum+1<p->n_nodes)    find_nodes(loc,vs,p,nnum*4+1);
-    if(/*loc->x >= p->node[nnum].x && loc->y <= p->node[nnum].y_right && */4*nnum+2<p->n_nodes)    find_nodes(loc,vs,p,nnum*4+2);
-    if(/*loc->x >= p->node[nnum].x && loc->y >= p->node[nnum].y_left  && */4*nnum+3<p->n_nodes)    find_nodes(loc,vs,p,nnum*4+3);
-    if(/*loc->x >= p->node[nnum].x && loc->y >= p->node[nnum].y_right && */4*nnum+4<p->n_nodes)    find_nodes(loc,vs,p,nnum*4+4);
+    if(4*nnum+1<p->n_nodes && !(!in_range(loc,p->node[nnum].x,p->node[nnum].y_left) 
+        && p->node[nnum].x < loc->x && p->node[nnum].y_left < loc->y))     find_nodes(loc,vs,p,nnum*4+1);
+    if(4*nnum+2<p->n_nodes && !(!in_range(loc,p->node[nnum].x,p->node[nnum].y_left) 
+        && p->node[nnum].x > loc->x && p->node[nnum].y_right < loc->y))    find_nodes(loc,vs,p,nnum*4+2);
+    if(4*nnum+3<p->n_nodes && !(!in_range(loc,p->node[nnum].x,p->node[nnum].y_left) 
+        && p->node[nnum].x < loc->x && p->node[nnum].y_left > loc->y))     find_nodes(loc,vs,p,nnum*4+3);
+    if(4*nnum+4<p->n_nodes && !(!in_range(loc,p->node[nnum].x,p->node[nnum].y_left) 
+        && p->node[nnum].x > loc->x && p->node[nnum].y_right > loc->y))    find_nodes(loc,vs,p,nnum*4+4);
+
+    // if(4*nnum+1<p->n_nodes)    find_nodes(loc,vs,p,nnum*4+1);
+    // if(4*nnum+2<p->n_nodes)    find_nodes(loc,vs,p,nnum*4+2);
+    // if(4*nnum+3<p->n_nodes)    find_nodes(loc,vs,p,nnum*4+3);
+    // if(4*nnum+4<p->n_nodes)    find_nodes(loc,vs,p,nnum*4+4);
 }
 
 void
@@ -63,9 +72,7 @@ up_update(graph_t* g, heap_t* h, int32_t index){
     if((index+1>=h->n_elts)&&(index%2==1)){    // if index+1 node out of bound and index node is odd
         if(g->vertex[*parent].from_src>g->vertex[*children1].from_src){
             swap(parent,children1);
-            // printf("(%d,%d) heapid:%d ",g->vertex[*parent].x,g->vertex[*parent].y,g->vertex[*parent].heap_id);
             swap(&(g->vertex[*parent].heap_id),&(g->vertex[*children1].heap_id));
-            // printf("(%d,%d) heapid:%d\n ",g->vertex[*parent].x,g->vertex[*parent].y, g->vertex[*parent].heap_id);
         }
     }
     else if(index%2==1){    // if index+1 node in bound and index node is even
@@ -92,16 +99,12 @@ up_update(graph_t* g, heap_t* h, int32_t index){
 }
 
 void down_update(graph_t* g, heap_t* h, int32_t index){
-    //printf("number of heap: %d\n",h->n_elts);
-    //printf("index 1:%d\t",index);
-    
     int32_t* parent = &(h->elt[index]);
     int32_t* children1 = &(h->elt[index * 2 + 1]);
     int32_t* children2 = &(h->elt[index * 2 + 2]);
     if(index*2+1 >= h->n_elts){
         return;
     }
-    //printf("index:%d,%d,%d,%d\t",index,g->vertex[*parent].from_src,g->vertex[*children1].from_src,g->vertex[*children2].from_src); 
     if(g->vertex[*parent].from_src > g->vertex[*children1].from_src
         && g->vertex[*children1].from_src < g->vertex[*children2].from_src){
         swap(parent,children1);
@@ -115,23 +118,6 @@ void down_update(graph_t* g, heap_t* h, int32_t index){
         down_update(g,h,index*2+2);
     }
 }
-
-void show_heap(graph_t* g,heap_t* h, int32_t elements){
-    int32_t i = 1;
-    while(i<elements){
-        for(int32_t j = 0; j<i; j++){
-            printf("%d ",g->vertex[h->elt[i+j-1]].from_src);
-        }
-        printf("\n");
-        i *= 2;
-    }
-}
-
-// void show_graph(graph_t* g){
-//     for(int32_t i=0;i<g->n_vertices;i++){
-//         printf("(id:%d):dis %d;pre %d\t",i,g->vertex[i].from_src,g->vertex[i].pred);
-//     }
-// }
 
 void 
 heap_init (graph_t* g, heap_t* h, vertex_set_t* src){
@@ -162,7 +148,6 @@ heap_pop(graph_t* g, heap_t* h){
     h->elt[0] = h->elt[h->n_elts-1];
     h->n_elts--;
     down_update(g,h,0);
-    //printf("%d \t",g->vertex[h->elt[0]].from_src);
     return u;
 }
 
@@ -175,54 +160,44 @@ dijkstra (graph_t* g, heap_t* h, vertex_set_t* src, vertex_set_t* dest,
     int32_t min_dest_id = UNDEFINED;
     int32_t current_id = UNDEFINED;
     heap_init(g,h,src);
-    //show_graph(g);
     while(h->n_elts!=0){
         int32_t u = heap_pop(g,h);
-        //static int32_t j = 1; if(j==2||j==3) show_heap(h,100);j++;
-        //printf("u: %d ",u);
-        //static int32_t j = 1; if(j==2) show_graph(g);j++;
         int32_t heap_index;
         for(int32_t i=0 ;i < g->vertex[u].n_neighbors; i++){
             alt = g->vertex[u].from_src + g->vertex[u].distance[i];
-            //printf("%d ",alt>MY_INFINITY);
             if(alt < g->vertex[g->vertex[u].neighbor[i]].from_src){
                 g->vertex[g->vertex[u].neighbor[i]].from_src = alt;
                 g->vertex[g->vertex[u].neighbor[i]].pred = u;
                 heap_index = g->vertex[g->vertex[u].neighbor[i]].heap_id;
-                //printf("heap_index: %d\n",heap_index);
                 up_update(g,h,heap_index);
             }
         }
     }
     // find the minimum distance to srouce set from destination set
-    //printf("%d\n",min_dest_id);
-    //show_graph(g);
     for(int32_t i=0; i < dest->count; i++){
-        // printf("g->vertex[dest->id[i]].from_src < min_dist: %d\n",g->vertex[dest->id[i]].from_src < min_dist);
         if(g->vertex[dest->id[i]].from_src < min_dist){
             min_dist = g->vertex[dest->id[i]].from_src;
             min_dest_id = dest->id[i];
         }
     }
     current_id = min_dest_id;
-    //printf("%d\n",min_dest_id);
     path->n_vertices = 0;
     path->tot_dist = g->vertex[min_dest_id].from_src;
-    //printf("current id: %d\n",current_id);
     if(g->vertex[min_dest_id].pred != UNDEFINED){   // the conidtion when the target fall in source is not considered
         while(current_id != UNDEFINED){
+            if(path->n_vertices + 1 > MAX_PATH_LENGTH){
+                return 0;
+            }
             path->id[path->n_vertices] = current_id;
-            path->n_vertices++;
+            path->n_vertices++; 
             current_id = g->vertex[current_id].pred;
+           
+        }
+        for(int32_t i=0;i<path->n_vertices/2;i++){
+            swap(&path->id[i],&path->id[path->n_vertices-1-i]);
         }
         return 1;
     }
     return 0;
 }
-
-
-//备注：测试不过的可能问题
-//最后的坐标点顺序不一致的问题应该是点放进去的顺序不一致
-//
-
 
--- mp9/mp9match.c
+++ mp9/mp9match.c
@@ -10,13 +10,13 @@ match_requests (graph_t* g, pyr_tree_t* p, heap_t* h,
 		request_t* r1, request_t* r2,
 		vertex_set_t* src_vs, vertex_set_t* dst_vs, path_t* path)
 {	
-	int32_t r;
+	int32_t r=0;
 	src_vs->count = 0;
 	dst_vs->count = 0;
 	find_nodes(&r1->from,src_vs,p,0);
 	trim_nodes(g,src_vs,&r2->from);
 	find_nodes(&r1->to,dst_vs,p,0);
 	trim_nodes(g,dst_vs,&r2->to);
-	r = dijkstra(g,h,src_vs,dst_vs,path);
+	if(src_vs->count && dst_vs->count)	r = dijkstra(g,h,src_vs,dst_vs,path);
 	return r;
 }
