ad20b6e5cfb2b1d09a5cdb9ec54071ea1d48aa1b
Raventhatfly <feiyangw.21@intl.zju.edu.cn>
2022-11-29 13:29:52 +0800

    change in mp9

--- mp9/mp9match.c
+++ mp9/mp9match.c
@@ -10,14 +10,13 @@ match_requests (graph_t* g, pyr_tree_t* p, heap_t* h,
 		request_t* r1, request_t* r2,
 		vertex_set_t* src_vs, vertex_set_t* dst_vs, path_t* path)
 {	
+	int32_t r;
+	src_vs->count = 0;
+	dst_vs->count = 0;
 	find_nodes(&r1->from,src_vs,p,0);
-	find_nodes(&r1->to,dst_vs,p,0);
-	find_nodes(&r2->from,src_vs,p,0);
-	find_nodes(&r2->to,dst_vs,p,0);
-	trim_nodes(g,src_vs,&r1->from);
-	trim_nodes(g,dst_vs,&r1->to);
 	trim_nodes(g,src_vs,&r2->from);
+	find_nodes(&r1->to,dst_vs,p,0);
 	trim_nodes(g,dst_vs,&r2->to);
-	dijkstra(g,h,src_vs,dst_vs,path);
-    return 0;
+	r = dijkstra(g,h,src_vs,dst_vs,path);
+	return r;
 }
